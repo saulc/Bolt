156b95f005d1570baa4db364d90693ec0dc4a5ef
Saul <saulc@users.noreply.github.com>
2023-04-25 22:30:12 -0700

    Tone Wheel Test
    servo interferance...

---
+++ Dev/stonewheel_test/irendstop.h
@@ -0,0 +1,70 @@
+#ifndef irendstop_h
+#define irendstop_h
+#include "Arduino.h"
+#include "PinChangeInterrupt.h"
+/*
+ * Code keys v1 - 2022/8/8 Saul C.
+ * button dobounce + double tap (needs work)
+ * external and pinchange interupt pints only.
+ * nano/micro only. not working for esp32. (tap timer only..)
+  */
+  
+class irendstop{
+  public: 
+  
+     irendstop(int p , void* callback,  bool isinterupt){
+      pin = p;
+      clickCallback = callback; 
+      isInterupt = isinterupt;
+     }
+    ~irendstop(){}
+    
+    void ini(){
+      pinMode(pin, INPUT_PULLUP); 
+      //no press n hold, alternate version for 'gaming response'
+      if(isInterupt) attachInterrupt(digitalPinToInterrupt(pin), clickCallback, CHANGE );  
+      else attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(pin), clickCallback, CHANGE); 
+      }
+     int getPin(){ 
+        return pin;
+     }
+      
+    //basic debounce//maybe add a double tap?
+    //double tap still triggers single tap, 
+    //solutions, wait for it or undo it. 
+//    for now use 'non conflicting' double taps.
+    long tap(){
+      long temp  = millis() - lastClick; 
+//      if(temp  > clickDelay){ 
+        rpm = 60000000/(32*(lastDur + flipDur));
+//        rpm = lastDur;
+        lastDur = flipDur;
+        flipDur = temp;
+        if(tik++ > 10000) tik = 0;
+        
+        if(tik % 64 == 0) rotations++;
+        
+        lastClick = millis();   
+        return temp; 
+    } 
+    int getTiks(){ return tik; }
+    int getRotations(){ return rotations; }
+
+    long getRpm(){
+      return rpm;
+    }
+    
+  private:
+    int dir = 0;
+    int tik = 0;
+    int rotations = 0;
+    int  rpm = 10;
+    int pin = 0;
+    long flipDur = 0, lastDur = 0;
+    long lastClick = 0; // for 3 interupt micros.
+    long clickDelay = 55;  //reduce for "gaming" response, false/extra key taps ok.
+    bool isInterupt;    //3 buttons isn't enough.....this may not work
+    void   *clickCallback; //just need to save a pointer and the int lib will do the rest
+    long doubleTapDelay = 350;  
+};    
+#endif
---
+++ Dev/stonewheel_test/stonewheel_test.ino
@@ -0,0 +1,94 @@
+
+
+#include <Servo.h>
+
+Servo servo0;
+Servo servo1; 
+#include "irendstop.h"
+
+void sensorChange();
+irendstop rpm0(3, sensorChange, true);
+int val = 7;
+const int ledPin = 13;   
+//volatile int tics = 0, lr = 0;
+volatile long lt = 0;
+volatile int tic = 1;
+
+void sensorChange(){ 
+//  if(tics % 32 == 0){
+//    state = !state;
+//    if(lr++ > 10) lr = 0;
+  lt = rpm0.tap();
+  if(tic++ > 1000) tic = 0;
+//  }
+//  lt = rpm0.tap();
+//  tics++;
+//   Serial.print(" tap = ");
+//  Serial.print(t);
+  
+}
+void setup() {
+  rpm0.ini();
+  // put your setup code here, to run once:
+//  pinMode(ledPin, OUTPUT);
+  servo0.attach(5);
+  delay(100);
+  servo1.attach(6);
+  delay(100);
+  Serial.begin(9600);
+   Serial.println("ACME tone wheel test ");
+   
+   servo1.write(  90+11 );
+    delay(311);  
+   servo1.write(  90 );
+    delay(311);  
+   servo1.write(  90-7 );
+    delay(311);  
+//   
+//     int s = 89+val; 
+//   servo0.write( 90);
+//   servo1.write(  90 );
+//    delay(11);  
+//   servo1.write(  s );
+//    delay(11);  
+//   servo0.write(  s );
+//    delay(311);  
+//    val *= -1;
+//    s = 89+val; 
+//   servo1.write(  s );
+//    delay(11);  
+//   servo0.write(  s );
+//    delay(311);  
+}
+
+void loop() {
+
+//  digitalWrite(13, state);
+  
+//   int s = map(val, 0, 1023, 0, 180); 
+  int s = 90+val;
+   if(tic%8==0) {
+    val *= -1;
+   servo0.write( 90);
+//   servo1.write(  90 );
+    delay(311);  
+//   servo1.write(  s );
+   servo0.write(  s );
+   tic++;
+   }else{
+   servo0.write(  s );
+//   servo1.write(  s );
+   }
+   long rpm = rpm0.getRpm();
+   
+   Serial.print("speed = ");
+  Serial.print(s);
+   Serial.print(" tic = ");
+  Serial.print(rpm0.getTiks());
+   Serial.print(" rotations = ");
+  Serial.print(rpm0.getRotations());
+   Serial.print(" lt = ");
+  Serial.print(lt);
+   Serial.print(" rpm = ");
+  Serial.println(rpm);
+}
